f8966bdfd773e50b4204b0482f396df1be9f5fb0
zenha.acevedo <zenha.acevedo@olxbr.com>
2022-04-03 00:03:17 -0300

    novo input para introduzir nome e fonte atualizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import bee from './images/bee.png';
 import './App.css';
+import Font from './fontes';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Font/>
         <section>
           <ul>
             <ol>
               <span>
                 Please, enter your full name below
               </span>
-            </ol>  
+            </ol>
             <ol>
               <span>
                 Only alphabetical characters are accepted
               </span>
             </ol>
+            <ol>
+              <input className="placeForName" placeholder="Full name"></input>
+            </ol>
           </ul>
         </section>
-        <img src={bee} alt="logo" className="Bee-Logo"/>
+        <img src={bee} alt="logo" className="Bee-Logo" />
         {/* <img src={logo} className="App-logo" alt="logo" />
         {/* <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/fontes.tsx
@@ -0,0 +1,9 @@
+function Font() {
+  return (
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;200;300&display=swap');
+</style>
+  )
+}
+
+export default Font;
