506ba5cf806816a38a94eb36248e2a50794b30a9
zenha.acevedo <zenha.acevedo@olxbr.com>
2022-04-06 15:34:08 -0300

    chamando api no priemiro card

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,26 +1,81 @@
-import React from "react";
-import './cards.css'
-import excluirImage from '../../images/Vector.png'
+import React, { useState, useEffect } from "react";
+import './cards.css';
+import excluirImage from '../../images/excluir.png';
+import telefone from '../../images/telefone.png';
+import tipo from '../../images/tipo.png';
+import completo from '../../images/completo.png';
+import axios from 'axios';
+
 function Cards() {
+
+  const [brewerieStreet, setBrewerieStreet] = useState('');
+  const [brewerieName, setBrewerieName] = useState('');
+  const [brewerieCidade, setBrewerieCidade] = useState('');
+  const [brewerieState, setBrewerieState] = useState('');
+  const [brewerieCountry, setBrewerieCountry] = useState('');
+  const [brewerieCep, setBrewerieCep] = useState('');
+  const [brewerieTelefone, setBrewerieTelefone] = useState('');
+  const [brewerieTipo, setBrewerieTipo] = useState('');
+
+
+  useEffect(() => {
+
+    axios.get('https://api.openbrewerydb.org/breweries?by_postal=44107')
+      .then((response) => {
+        const brewerieChegando = response.data;
+        setBrewerieStreet(brewerieChegando[1].street);
+        setBrewerieName(brewerieChegando[1].name);
+        setBrewerieCidade(brewerieChegando[1].city);
+        setBrewerieState(brewerieChegando[1].state);
+        setBrewerieCountry(brewerieChegando[1].country);
+        setBrewerieCep(brewerieChegando[1].postal_code);
+        setBrewerieTelefone(brewerieChegando[1].phone);
+        setBrewerieTipo(brewerieChegando[1].brewery_type);
+      });
+  }, []);
 
   return (
     <div className="divCard">
       <div className="headcard">
         <div className="nomeBrewerie">
-          <span><b>10-56 Brewering Company l</b></span>
+          <span><b>{brewerieName}</b></span>
         </div>
         <div>
-          <img className="excluirIcon" src={excluirImage}/>
+          <img className="excluirIcon" src={excluirImage} />
         </div>
       </div>
       <div class="container">
         <div>
-          <span>John Doe</span>
+          <span>{brewerieStreet} </span>
         </div>
         <div>
-          <span>Architect e Engineer</span>
+          <span>{brewerieCidade} {brewerieState} - {brewerieCountry} </span>
         </div>
       </div>
+      <table className="tags">
+        <tr>
+          <td>
+            <img src={tipo} />
+            <p className="teste">
+              {brewerieTipo}
+            </p>
+          </td>
+          <td>
+            <img src={completo} />
+            <p className="teste">
+              {brewerieCep}
+            </p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <img src={telefone} />
+            <p className="teste">
+              {brewerieTelefone}
+            </p>
+          </td>
+        </tr>
+      </table>
     </div>
   );
 };
